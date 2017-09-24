b0c767ee39b1b723732f6056fba6bdb025171e73
Bret Farley <bretfarley12@mittymonarchl.com>
2017-09-24 01:20:30 -0700

    initial

---
+++ birthday.js
@@ -0,0 +1,14 @@
+var daysTill = 40;
+while(daysTill > 30){
+    console.log("It's still ", daysTill, "days until my birthday...wompwompwomp");
+    daysTill --;
+}
+while(daysTill > 5){
+    console.log("Hey! It's only", daysTill, "days until my birthday!");
+    daysTill --;
+}
+while(daysTill > 0){
+    console.log("HOLY CRAP IT'S ", daysTill, "DAYS TO MY BIRTHDAY!");
+    daysTill --;
+}
+console.log("AIRHORNS PEWPEWPEWPEW HAPPY BIRTHDAY PEWPEWPEWPEWPEW");
